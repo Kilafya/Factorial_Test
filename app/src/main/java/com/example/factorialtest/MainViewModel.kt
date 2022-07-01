fdd76bd5945a203a385134983e9fae9cb2a961f0
drayl <sunrise.161@mail.ru>
2022-07-01 14:39:17 +0300

    finished

--- app/src/main/java/com/example/factorialtest/MainViewModel.kt
+++ app/src/main/java/com/example/factorialtest/MainViewModel.kt
@@ -4,10 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import java.math.BigInteger
 import kotlin.concurrent.thread
 import kotlin.coroutines.suspendCoroutine
@@ -18,29 +15,62 @@ class MainViewModel: ViewModel() {
     val state: LiveData<State>
         get() = _state
 
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + CoroutineName("my coroutine scope"))
+
     fun calculate(value: String?) {
         if (value.isNullOrBlank()) {
             _state.value = Error
             return
         }
         _state.value = Progress
-        viewModelScope.launch {
+        coroutineScope.launch {
             val number = value.toLong()
-            val result = factorial(number)
-            delay(1000)
+            val result = withContext(Dispatchers.Default) {
+                factorial(number)
+            }
             _state.value = Factorial(result)
         }
     }
 
-    private suspend fun factorial(number: Long): String {
-        return withContext(Dispatchers.Default) {
-            var result = BigInteger.ONE
-            for (i in 1..number) {
-                result = result.multiply(BigInteger.valueOf(i))
-            }
-            result.toString()
+//    fun calculate(value: String?) {
+//        if (value.isNullOrBlank()) {
+//            _state.value = Error
+//            return
+//        }
+//        _state.value = Progress
+//        viewModelScope.launch {
+//            val number = value.toLong()
+//            withContext(Dispatchers.Default) {
+//                val result = factorial(number)
+//                withContext(Dispatchers.Main) {
+//                    _state.value = Factorial(result)
+//                }
+//            }
+//        }
+//    }
+
+    private fun factorial(number: Long): String {
+        var result = BigInteger.ONE
+        for (i in 1..number) {
+            result = result.multiply(BigInteger.valueOf(i))
         }
+        return result.toString()
     }
+
+    override fun onCleared() {
+        super.onCleared()
+        coroutineScope.cancel()
+    }
+
+//    private suspend fun factorial(number: Long): String {
+//        return withContext(Dispatchers.Default) {
+//            var result = BigInteger.ONE
+//            for (i in 1..number) {
+//                result = result.multiply(BigInteger.valueOf(i))
+//            }
+//            result.toString()
+//        }
+//    }
 
 //    private suspend fun factorial(number: Long): String {
 //        return suspendCoroutine {
